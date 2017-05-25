4ddfe5aa89ced2c4d4c0126b7872a720a596ece6
angelmarquesadacelular <angelcamposalejo@gmail.com>
2017-05-25 11:50:23 -0500

    ejercicio printpreview

--- Gente/Gente.cpp
+++ Gente/Gente.cpp
@@ -8,22 +8,25 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR cmdLine, int cmdSho
 
 void Gente::Window_Open(Win::Event& e)
 {
-	//________________________________________________________ bchart1
-	bchart1.Bars.Add(L"May", RGB(230, 50, 50), 20.0);
-	bchart1.Bars.Add(L"June", RGB(50, 230, 50), 40.0);
-	bchart1.Bars.Add(L"July", RGB(50, 50, 230), 80.0);
-	bchart1.MaxY = 100.0;
-	bchart1.Text = L"Sales";
+	//________________________________________________________ pie1
+	pie1.Pies.Add(L"Adult", RGB(250, 70, 70), 200.0);
+	pie1.Pies.Add(L"Children", RGB(70, 70, 250), 100.0);
+	pie1.Pies.Add(L"Teens", RGB(70, 250, 70), 50.0);
+	//________________________________________________________ lv1
+	lv1.Cols.Add(0, LVCFMT_LEFT, 100, L"Day");
+	lv1.Cols.Add(1, LVCFMT_RIGHT, 200, L"Activity");
+	lv1.Items.Add(0, L"Monday");
+	lv1.Items[0][1].Text = L"Math Class";
 }
 
 
 void Gente::btImprimir_Click(Win::Event& e)
 {
 	Win::PrintDoc doc;
-	doc.Create(L"Sales Report");
-	doc.Add(9000, 1, bchart1);
-	doc.Add(500, tbx1.GetPrintLineCount(doc, 500), tbx1);
-	Win::PrintDlg_ dlg;
-	dlg.BeginDialog(hWnd, &doc);
+	doc.Create(L"Produc Report");
+	doc.Add(10000, 1, pie1);
+	doc.Add(500, lv1.Items.Count + 1, lv1);
+	Win::PrintPreviewDlg dlg;
+	dlg.BeginDialog_(hWnd, &doc);
 }
 
--- Gente/backup.h
+++ Gente/backup.h
@@ -13,14 +13,14 @@ public:
 protected:
 	//______ Wintempla GUI manager section begin: DO NOT EDIT AFTER THIS LINE
 	Win::Button btImprimir;
-	Win::BarChart bchart1;
-	Win::Textbox tbx1;
+	Win::PieChart pie1;
+	Win::ListView lv1;
 protected:
 	Win::Gdi::Font fontArial014A;
 	void GetDialogTemplate(DLGTEMPLATE& dlgTemplate)
 	{
 		dlgTemplate.cx = Sys::Convert::PixelToDlgUnitX(469);
-		dlgTemplate.cy = Sys::Convert::PixelToDlgUnitY(311);
+		dlgTemplate.cy = Sys::Convert::PixelToDlgUnitY(365);
 		dlgTemplate.style = WS_CAPTION | WS_POPUP | WS_SYSMENU | WS_VISIBLE | DS_CENTER | DS_MODALFRAME;
 	}
 	//_________________________________________________
@@ -28,12 +28,13 @@ protected:
 	{
 		this->Text = L"Gente";
 		btImprimir.Create(NULL, L"Imprimir", WS_CHILD | WS_TABSTOP | WS_VISIBLE | BS_PUSHBUTTON | BS_CENTER | BS_VCENTER, 376, 39, 86, 28, hWnd, 1000);
-		bchart1.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_VISIBLE, 24, 18, 348, 237, hWnd, 1001);
-		tbx1.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_TABSTOP | WS_VISIBLE | ES_AUTOHSCROLL | ES_LEFT | ES_WINNORMALCASE, 21, 272, 344, 32, hWnd, 1002);
+		pie1.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_VISIBLE, 25, 21, 336, 230, hWnd, 1001);
+		lv1.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_TABSTOP | WS_VISIBLE | LVS_REPORT, 26, 268, 342, 90, hWnd, 1002);
+		lv1.SetExtStyle(LVS_EX_FULLROWSELECT | LVS_EX_GRIDLINES, LVS_EX_FULLROWSELECT | LVS_EX_GRIDLINES);
 		fontArial014A.Create(L"Arial", 14, false, false, false, false);
 		btImprimir.Font = fontArial014A;
-		bchart1.Font = fontArial014A;
-		tbx1.Font = fontArial014A;
+		pie1.Font = fontArial014A;
+		lv1.Font = fontArial014A;
 	}
 	//_________________________________________________
 	void btImprimir_Click(Win::Event& e);
