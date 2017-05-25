12b7d28d23687b63ff0c6a3df34bb8bda574eda3
angelmarquesadacelular <angelcamposalejo@gmail.com>
2017-05-25 11:36:27 -0500

    ejercicio print dialog

--- Gente/Gente.cpp
+++ Gente/Gente.cpp
@@ -8,35 +8,22 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR cmdLine, int cmdSho
 
 void Gente::Window_Open(Win::Event& e)
 {
-
-	//________________________________________________________ xy1
-	xy1.CaptionX = L"Axis X";
-	xy1.CaptionY = L"Axis Y";
-	xy1.MinX = 0.0;
-	xy1.MaxX = 6.28;
-	xy1.MinY = -1.0;
-	xy1.MaxY = 1.0;
-	xy1.Graphs.Add(100);
-	for (int i = 0; i<100; i++)
-	{
-		xy1.Graphs[0][i].x = i*6.28 / 100;
-		xy1.Graphs[0][i].y = sin(i*6.18 / 100);
-	}
-	//xy1.Graphs[0].Color = RGB(0, 0, 255);
-	//xy1.Graphs[0].Type = Win::Graph::circle;
-	//xy1.Graphs[0].Caption = L"My Function";
-	xy1.RefreshAll();
+	//________________________________________________________ bchart1
+	bchart1.Bars.Add(L"May", RGB(230, 50, 50), 20.0);
+	bchart1.Bars.Add(L"June", RGB(50, 230, 50), 40.0);
+	bchart1.Bars.Add(L"July", RGB(50, 50, 230), 80.0);
+	bchart1.MaxY = 100.0;
+	bchart1.Text = L"Sales";
 }
 
 
-
-void Gente::btColor_Click(Win::Event& e)
+void Gente::btImprimir_Click(Win::Event& e)
 {
-	Win::ColorDlg dlg;
-	dlg.SelectedColor = xy1.BackColor;
-	if (dlg.BeginDialog(hWnd) == true)
-	{
-		xy1.BackColor = dlg.SelectedColor;
-	}
+	Win::PrintDoc doc;
+	doc.Create(L"Sales Report");
+	doc.Add(9000, 1, bchart1);
+	doc.Add(500, tbx1.GetPrintLineCount(doc, 500), tbx1);
+	Win::PrintDlg_ dlg;
+	dlg.BeginDialog(hWnd, &doc);
 }
 
--- Gente/Gente.h
+++ Gente/Gente.h
@@ -12,33 +12,36 @@ public:
 	}
 protected:
 	//______ Wintempla GUI manager section begin: DO NOT EDIT AFTER THIS LINE
-	Win::Button btColor;
-	Win::XyChart xy1;
+	Win::Button btImprimir;
+	Win::BarChart bchart1;
+	Win::Textbox tbx1;
 protected:
 	Win::Gdi::Font fontArial014A;
 	void GetDialogTemplate(DLGTEMPLATE& dlgTemplate)
 	{
 		dlgTemplate.cx = Sys::Convert::PixelToDlgUnitX(469);
-		dlgTemplate.cy = Sys::Convert::PixelToDlgUnitY(250);
+		dlgTemplate.cy = Sys::Convert::PixelToDlgUnitY(304);
 		dlgTemplate.style = WS_CAPTION | WS_POPUP | WS_SYSMENU | WS_VISIBLE | DS_CENTER | DS_MODALFRAME;
 	}
 	//_________________________________________________
 	void InitializeGui()
 	{
 		this->Text = L"Gente";
-		btColor.Create(NULL, L"Color", WS_CHILD | WS_TABSTOP | WS_VISIBLE | BS_PUSHBUTTON | BS_CENTER | BS_VCENTER, 376, 39, 86, 28, hWnd, 1000);
-		xy1.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_VISIBLE, 19, 33, 294, 210, hWnd, 1001);
+		btImprimir.Create(NULL, L"Imprimir", WS_CHILD | WS_TABSTOP | WS_VISIBLE | BS_PUSHBUTTON | BS_CENTER | BS_VCENTER, 376, 39, 86, 28, hWnd, 1000);
+		bchart1.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_VISIBLE, 24, 18, 348, 237, hWnd, 1001);
+		tbx1.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_TABSTOP | WS_VISIBLE | ES_AUTOHSCROLL | ES_LEFT | ES_WINNORMALCASE, 21, 272, 344, 25, hWnd, 1002);
 		fontArial014A.Create(L"Arial", 14, false, false, false, false);
-		btColor.Font = fontArial014A;
-		xy1.Font = fontArial014A;
+		btImprimir.Font = fontArial014A;
+		bchart1.Font = fontArial014A;
+		tbx1.Font = fontArial014A;
 	}
 	//_________________________________________________
-	void btColor_Click(Win::Event& e);
+	void btImprimir_Click(Win::Event& e);
 	void Window_Open(Win::Event& e);
 	//_________________________________________________
 	bool EventHandler(Win::Event& e)
 	{
-		if (btColor.IsEvent(e, BN_CLICKED)) { btColor_Click(e); return true; }
+		if (btImprimir.IsEvent(e, BN_CLICKED)) { btImprimir_Click(e); return true; }
 		return false;
 	}
 };
